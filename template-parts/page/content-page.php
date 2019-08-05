e1f83143e543508fa4722a02bd5634a6e9d01b0e
Ellen Bauer <ellen.bauer@elmastudio.de>
2019-08-06 00:29:55 +0700

    New theme URL

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -9,14 +9,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header cf">
+	<header class="entry-header">
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<?php aino_edit_link(); ?>
 
 	</header><!-- .entry-header -->
-	<div class="entry-content col cf">
+	<div class="entry-content col">
 		<?php
 			the_content();
 			wp_link_pages(
