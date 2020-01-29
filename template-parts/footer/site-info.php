32feb04d0e6602c4bc8c50446cfe07bc09c98544
Ellen Bauer <ellen.bauer@elmastudio.de>
2020-01-29 23:19:33 +1300

    Footer credit optimized

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,13 +10,23 @@
 
 <div class="site-info">
 	<?php $blog_info = get_bloginfo( 'name' ); ?>
-	<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-			<?php endif; ?>
-		<a href="<?php echo esc_url( __( 'https://wpaino.com', 'aino' ) ); ?>" class="imprint">
+		<?php if ( ! empty( $blog_info ) ) : ?>
+
+			<span class="footer-copyright">&copy;
+				<?php
+				echo date_i18n(
+					/* translators: Copyright date format, see https://secure.php.net/date */
+					_x( 'Y', 'copyright date format', 'aino' )
+				);
+				?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>.
+			</span><!-- .footer-copyright -->
+
+		<?php endif; ?>
+		<a href="<?php echo esc_url( __( 'https://wpaino.com', 'aino' ) ); ?>" class="powered-by">
 			<?php
 			/* translators: %s: theme name */
-			printf( esc_html__( 'Proudly powered by %s.', 'aino' ), 'Aino' );
+			printf( esc_html__( 'Powered by %s.', 'aino' ), 'Aino' );
 			?>
 		</a>
 		<?php
