b3668c4f21308f67bb7fc72cfe4573b81b5ad97a
Ellen Bauer <ellen.bauer@elmastudio.de>
2021-08-09 17:07:29 +1200

    Enhance: WooCommerce account

--- functions.php
+++ functions.php
@@ -219,6 +219,60 @@ function aino_separate_registration_form() {
 }
 add_shortcode( 'woocommerce_registration_form', 'aino_separate_registration_form' );
 
+// Separate WooCommerce User Login form
+function aino_separate_login_form() {
+	if ( is_admin() ) return;
+	if ( is_user_logged_in() ) return;
+	ob_start();
+
+	// Form copied from /woocommerce/myaccount/form-login.php (version 4.1.0) Update as needed.
+	do_action( 'woocommerce_before_customer_login_form' ); ?>
+
+	<?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
+
+	<div class="u-columns col2-set" id="customer_login">
+
+		<div class="u-column1 col-1">
+
+			<?php endif; ?>
+
+			<form class="woocommerce-form woocommerce-form-login login" method="post">
+
+			<?php do_action( 'woocommerce_login_form_start' ); ?>
+
+			<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+				<label for="username"><?php esc_html_e( 'Username or email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+				<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+			</p>
+			<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+				<label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+				<input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password" />
+			</p>
+
+			<?php do_action( 'woocommerce_login_form' ); ?>
+
+			<p class="form-row">
+				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+					<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
+				</label>
+				<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+				<button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
+			</p>
+			<p class="woocommerce-LostPassword lost_password">
+				<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
+			</p>
+
+			<?php do_action( 'woocommerce_login_form_end' ); ?>
+
+		</form>
+
+	<?php
+
+	return ob_get_clean();
+
+}
+add_shortcode( 'woocommerce_login_form', 'aino_separate_login_form' );
+
 // Custom template tags for this theme.
 require get_template_directory() . '/inc/template-tags.php';
 
