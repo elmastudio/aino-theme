b51080c09f96564ea42333593f608774d0efbb19
Ellen Bauer <ellen.bauer@elmastudio.de>
2021-11-01 10:45:57 +1300

    CSS updates

--- functions.php
+++ functions.php
@@ -19,9 +19,6 @@ if ( ! function_exists( 'aino_setup' ) ) :
 		// Add support for Block Styles.
 		add_theme_support( 'wp-block-styles' );
 
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
 		// Enqueue editor styles.
 		add_editor_style( array( 'assets/build/css/editor.css', aino_fonts_url() ) );
 
