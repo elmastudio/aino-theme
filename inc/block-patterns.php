8f63dddf77eaae1a6a017be5de4d8c185e22538a
Ellen Bauer <ellen.bauer@elmastudio.de>
2021-11-09 10:56:15 +1300

    Patterns updates

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -13,105 +13,105 @@ if ( ! function_exists( 'aino_register_block_patterns' ) ) :
 
 			register_block_pattern_category(
 				'aino-hero',
-				array( 'label' => __( 'Aino Hero', 'aino' ) )
+				array( 'label' => __( 'Hero', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-feature',
-				array( 'label' => __( 'Aino Feature', 'aino' ) )
+				array( 'label' => __( 'Feature', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-team',
-				array( 'label' => __( 'Aino Team', 'aino' ) )
+				array( 'label' => __( 'Team', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-portfolio',
-				array( 'label' => __( 'Aino Portfolio', 'aino' ) )
+				array( 'label' => __( 'Portfolio', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-text',
-				array( 'label' => __( 'Aino Text', 'aino' ) )
+				array( 'label' => __( 'Text', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-contact',
-				array( 'label' => __( 'Aino Contact', 'aino' ) )
+				array( 'label' => __( 'Contact', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-query',
-				array( 'label' => __( 'Aino Query', 'aino' ) )
+				array( 'label' => __( 'Query', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-banner',
-				array( 'label' => __( 'Aino Banner', 'aino' ) )
+				array( 'label' => __( 'Banner', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-header',
-				array( 'label' => __( 'Aino Header', 'aino' ) )
+				array( 'label' => __( 'Header', 'aino' ) )
 			);
 
 			register_block_pattern_category(
 				'aino-footer',
-				array( 'label' => __( 'Aino Footer', 'aino' ) )
+				array( 'label' => __( 'Footer', 'aino' ) )
 			);
 		}
 
 		if ( function_exists( 'register_block_pattern' ) ) {
 			$block_patterns = array(
-				'hero-text-img-below-light',
-				'hero-text-img-below-dark',
-				'feature-grid-one-two-col-light',
-				'feature-grid-one-two-col-dark',
-				'feature-heading-two-text-img-dark',
-				'feature-heading-two-text-img-light',
-				'feature-text-right-img-left-dark',
-				'feature-text-right-img-left-light',
-				'feature-text-left-img-right-dark',
-				'feature-text-left-img-right-light',
-				'team-four-img-light',
-				'team-four-img-dark',
-				'team-three-img-light',
-				'team-three-img-dark',
-				'team-three-img-list-light',
-				'team-three-img-list-dark',
-				'team-two-img-light',
-				'team-two-img-dark',
-				'team-one-img-small-light',
-				'team-one-img-small-dark',
-				'team-one-img-large-light',
-				'team-one-img-large-dark',
-				'portfolio-four-img-dark',
-				'portfolio-four-img-light',
-				'text-heading-centered-dark',
-				'text-heading-centered-light',
-				'text-big-heading-left-text-right-dark',
-				'text-big-heading-left-text-right-light',
-				'text-centered-paragraph-dark',
-				'text-centered-paragraph-light',
-				'text-small-heading-left-text-right-dark',
-				'text-small-heading-left-text-right-light',
-				'text-big-headline-two-col-text-dark',
-				'text-big-headline-two-col-text-light',
-				'text-five-col-text-dark',
-				'text-five-col-text-light',
-				'contact-big-heading-three-col-light',
-				'contact-big-heading-three-col-dark',
-				'query-three-col-light',
-				'query-three-col-dark',
-				'banner-default-light',
-				'banner-default-dark',
-				'banner-wide-light',
-				'banner-wide-dark',
-				'header-centered-slim-light',
-				'header-centered-slim-dark',
-				'footer-five-col-sitelogo-light',
-				'footer-five-col-sitelogo-dark',
+				'free/hero-text-img-below-light',
+				'free/hero-text-img-below-dark',
+				'free/feature-grid-one-two-col-light',
+				'free/feature-grid-one-two-col-dark',
+				'free/feature-heading-two-text-img-dark',
+				'free/feature-heading-two-text-img-light',
+				'free/feature-text-right-img-left-dark',
+				'free/feature-text-right-img-left-light',
+				'free/feature-text-left-img-right-dark',
+				'free/feature-text-left-img-right-light',
+				'free/team-four-img-light',
+				'free/team-four-img-dark',
+				'free/team-three-img-light',
+				'free/team-three-img-dark',
+				'free/team-three-img-list-light',
+				'free/team-three-img-list-dark',
+				'free/team-two-img-light',
+				'free/team-two-img-dark',
+				'free/team-one-img-small-light',
+				'free/team-one-img-small-dark',
+				'free/team-one-img-large-light',
+				'free/team-one-img-large-dark',
+				'free/portfolio-four-img-dark',
+				'free/portfolio-four-img-light',
+				'free/text-heading-centered-dark',
+				'free/text-heading-centered-light',
+				'free/text-big-heading-left-text-right-dark',
+				'free/text-big-heading-left-text-right-light',
+				'free/text-centered-paragraph-dark',
+				'free/text-centered-paragraph-light',
+				'free/text-small-heading-left-text-right-dark',
+				'free/text-small-heading-left-text-right-light',
+				'free/text-big-headline-two-col-text-dark',
+				'free/text-big-headline-two-col-text-light',
+				'free/text-five-col-text-dark',
+				'free/text-five-col-text-light',
+				'free/contact-big-heading-three-col-light',
+				'free/contact-big-heading-three-col-dark',
+				'free/query-three-col-light',
+				'free/query-three-col-dark',
+				'free/banner-default-light',
+				'free/banner-default-dark',
+				'free/banner-wide-light',
+				'free/banner-wide-dark',
+				'free/header-centered-slim-light',
+				'free/header-centered-slim-dark',
+				'free/footer-five-col-sitelogo-light',
+				'free/footer-five-col-sitelogo-dark',
 			);
 
 			foreach ( $block_patterns as $block_pattern ) {
