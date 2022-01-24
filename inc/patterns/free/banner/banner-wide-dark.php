822d263bfa457068b9582e37bc57f2dc465d962e
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-01-25 12:55:53 +1300

    Pattern bugfixes

--- inc/patterns/free/banner/banner-wide-dark.php
+++ inc/patterns/free/banner/banner-wide-dark.php
@@ -6,19 +6,19 @@
 return array(
 	'title'      => __( 'Wide content width (Dark)', 'aino' ),
 	'categories' => array( 'banner' ),
-	'content'    => '<!-- wp:ainoblocks/flexbox {"align":"wide","className":"fullwidth","flexDirectionMobile":"direction__colrev__m","justifyContentTablet":"justify__between__t","justifyContentMobile":"justify__between__m","alignItemsDesktop":"align-items__center__d","alignItemsTablet":"align-items__center__t","alignItemsMobile":"align-items__center__m","alignContentDesktop":"align-cont__between__d","backgroundColor":"font-secondary"} -->
-	<div class="wp-block-ainoblocks-flexbox alignwide fullwidth direction__row__d direction__row__t direction__colrev__m nowrap__d nowrap_t nowrap_m justify__flexstart__d justify__between__t justify__between__m align-items__center__d align-items__center__t align-items__center__m align-cont__between__d align-cont__stretch__t align-cont__stretch__m shadow-none wide has-font-secondary-background-color has-background"><!-- wp:ainoblocks/flex-item {"alignSelfDesktop":"align__auto__d","className":"desktop__width__50","paddingTop":11,"paddingBottom":11,"paddingLeft":13,"paddingRight":13} -->
-	<div class="wp-block-ainoblocks-flex-item align__auto__d auto desktop__width__50 pt__11 pb__11 pl__13 pr__13"><!-- wp:heading {"level":3,"textColor":"background-primary","fontSize":""text-xxxl"","paddingTop":0,"paddingBottom":8,"paddingLeft":0,"paddingRight":0} -->
-	<h3 class="has-background-primary-color has-text-color has-text-xxxl-font-size pb__8">Build a shiny new website</h3>
-	<!-- /wp:heading -->
+	'content'    => '<!-- wp:ainoblocks/flexbox {"align":"wide","flexDirectionMobile":"direction__colrev__m","justifyContentTablet":"justify__between__t","justifyContentMobile":"justify__between__m","alignItemsDesktop":"align-items__center__d","alignItemsTablet":"align-items__center__t","alignItemsMobile":"align-items__center__m","alignContentDesktop":"align-cont__between__d","className":"fullwidth","backgroundColor":"font-secondary"} -->
+	<div class="wp-block-ainoblocks-flexbox alignwide direction__row__d direction__row__t direction__colrev__m nowrap__d nowrap_t nowrap_m justify__flexstart__d justify__between__t justify__between__m align-items__center__d align-items__center__t align-items__center__m align-cont__between__d align-cont__stretch__t align-cont__stretch__m shadow-none wide fullwidth has-font-secondary-background-color has-background"><!-- wp:ainoblocks/flex-item {"alignSelfDesktop":"align__auto__d","className":"desktop__width__50","paddingTop":11,"paddingBottom":11,"paddingLeft":13,"paddingRight":13} -->
+	<div class="wp-block-ainoblocks-flex-item align__auto__d auto desktop__width__50 pt__11 pb__11 pl__13 pr__13"><!-- wp:heading {"textColor":"variant-font-primary","fontSize":"text-xxxl","paddingBottom":8""} -->
+		<h2 class="has-variant-font-primary-color has-text-color has-text-xxxl-font-size pb__8 has-font-primary-color" id="build-a-shiny-new-website">Build a shiny new website</h2>
+		<!-- /wp:heading -->
 	
 	<!-- wp:paragraph {"textColor":"variant-font-secondary","fontSize":"text-xs","paddingBottom":8} -->
 	<p class="has-variant-font-secondary-color has-text-color has-text-xs-font-size pb__8">Aino is a growing system of modular design templates, patterns and themes&nbsp;for&nbsp;WordPress.</p>
 	<!-- /wp:paragraph -->
 	
 	<!-- wp:ainoblocks/multiple-buttons -->
-	<div class="wp-block-ainoblocks-multiple-buttons"><div class="wp-block-ainoblocks-multiple-buttons__inner"><!-- wp:ainoblocks/button {"borderRadius":200,"label":"Get started","backgroundColor":"variant-font-tertiary"} -->
-	<div class="wp-block-ainoblocks-button has-button-color-primary-color has-variant-font-tertiary-background-color has-text-color has-background" style="border-radius:200px"><a class="wp-block-ainoblocks-button__link size__m 200" href="https://ainoblocks.io/" style="border-radius:200px">Get started</a></div>
+	<div class="wp-block-ainoblocks-multiple-buttons"><div class="wp-block-ainoblocks-multiple-buttons__inner"><!-- wp:ainoblocks/button {"borderRadius":200,"label":"Get started","backgroundColor":"variant-font-tertiary","className":"has-button-color-primary-color has-text-color"} -->
+	<div class="wp-block-ainoblocks-button has-button-color-primary-color has-text-color has-variant-font-tertiary-background-color has-background" style="border-radius:200px"><a class="wp-block-ainoblocks-button__link size__m 200" href="https://ainoblocks.io/" style="border-radius:200px">Get started</a></div>
 	<!-- /wp:ainoblocks/button --></div></div>
 	<!-- /wp:ainoblocks/multiple-buttons --></div>
 	<!-- /wp:ainoblocks/flex-item -->
