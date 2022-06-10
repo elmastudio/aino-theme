d3e46c5a5a3b465c8a22eb806a62fd9661edb9a3
Ellen Bauer <ellen.bauer@elmastudio.de>
2022-06-10 20:31:33 +1200

    Update Header patterns

--- inc/patterns/free/header/header-dark.php
+++ inc/patterns/free/header/header-dark.php
@@ -8,7 +8,7 @@ return array(
 	'blockTypes' => array( 'core/template-part/header' ),
 	'content'    => '<!-- wp:group {"backgroundColor":"variant-background-primary","className":"site-header header-dark"} --><div class="wp-block-group site-header header-dark has-variant-background-primary-background-color has-background"><!-- wp:group {"className":"header-content,"paddingTop":5"} --><div class="wp-block-group header-content pt__5"><!-- wp:site-title {"fontSize":"text-l","textColor":"variant-font-secondary"} /-->
 	<!-- wp:ainoblocks/flexbox {"justifyContentDesktop":"justify__flexend__d","justifyContentTablet":"justify__flexend__t","justifyContentMobile":"justify__flexend__m"} --><div class="wp-block-ainoblocks-flexbox direction__row__d direction__row__t direction__row__m nowrap__d nowrap_t nowrap_m justify__flexend__d justify__flexend__t justify__flexend__m align-items__stretch__d align-items__stretch__t align-itms__stretch__m align-cont__stretch__d align-cont__stretch__t align-cont__stretch__m shadow-none">
-	<!-- wp:navigation {"textColor":"variant-font-primary","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"fontSize":"text-s","style":{"spacing":{"blockGap":"1.5rem"}}} -->
+	<!-- wp:navigation {"textColor":"variant-font-primary","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"},"fontSize":"text-s","style":{"spacing":{"blockGap":"1.5rem"}}} -->
 	<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
 <!-- /wp:navigation -->
 	<!-- wp:social-links {"iconColor":"variant-font-secondary","iconColorValue":"#f4f4f4","openInNewTab":true,"size":"has-normal-icon-size","className":"is-style-logos-only"} -->
